649061453fe9617fc7449b5d717504532af39c50
Mach-web <mkanderson771@gmail.com>
2025-03-23 02:10:51 +0300

    Load gemini model

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notestoaudio/routes/app_routings.dart';
 import 'package:notestoaudio/themes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_vertexai/firebase_vertexai.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+// Initialize the Vertex AI service and the generative model
+// Specify a model that supports your use case
+final model =
+      FirebaseVertexAI.instance.generativeModel(model: 'gemini-2.0-flash');
   runApp(const MyApp());
 }
 
