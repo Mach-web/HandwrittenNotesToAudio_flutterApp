255c71625ab0c02a77832db2619c0ec689f168f8
Mach-web <mkanderson771@gmail.com>
2025-03-27 00:07:44 +0300

    Configured .dotenv

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
-const functions = require("firebase-functions");
+require("dotenv").config();
+const functions = require("firebase-functions"); 
 const axios = require("axios");
 
 exports.transcribeAudio = functions.https.onRequest(async (req, res) => {
     try {
-        const apiKey = "process.env.API_KEY"; // Replace with your key
+        const apiKey = process.env.API_KEY; // Replace with your key
         const audioURL = req.body.audioURL; // Get the file URL from request
 
         const response = await axios.post(
